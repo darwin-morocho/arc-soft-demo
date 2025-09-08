b3988bc4333d2af89e325ffe42b232c13969af23
Darwin Morocho <darwin.morocho@icloud.com>
2025-09-07 21:42:49 -0500

    feat: Enhance biometric authentication screen with camera preview and layout adjustments

--- lib/presentation/screens/biometric_auth/biometric_auth_screen.dart
+++ lib/presentation/screens/biometric_auth/biometric_auth_screen.dart
@@ -1,11 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
+import 'biometric_auth_controller.dart';
+import 'widgets/camera_preview.dart';
+
 class BiometricAuthScreen extends HookWidget {
   const BiometricAuthScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final controller = useMemoized(() => BiometricAuthController());
+
+    useEffect(() {
+      return controller.dispose;
+    }, []);
+
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: [
+              SizedBox(height: 20),
+              CameraPreview(),
+              SizedBox(height: 20),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/presentation/screens/biometric_auth/widgets/camera_preview.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CameraPreview extends StatelessWidget {
+  const CameraPreview({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 1,
+      child: ClipOval(
+        child: Container(
+          color: Colors.grey[800],
+        ),
+      ),
+    );
+  }
+}
