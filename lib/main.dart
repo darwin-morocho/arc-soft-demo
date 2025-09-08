1f1dd3df82b0f6a853fe10d47772f7be04559efb
Darwin Morocho <darwin.morocho@icloud.com>
2025-09-07 21:45:22 -0500

    feat: Implement permission requests for camera, phone, and storage in main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 import 'presentation/screens/biometric_auth/biometric_auth_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Permission requests must implement better handling in a production app
+  // this is just for demo purposes, you should check the permission status
+  // before interact with camera, phone or storage
+  await Permission.camera.request();
+  await Permission.phone.request();
+  await Permission.storage.request();
+
   runApp(const MyApp());
 }
 
